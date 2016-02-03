422b212730a16a056c4784c1ffa18b57c6975a3f
HarkiranG <harkiran988@gmail.com>
2016-02-02 20:05:19 -0500

    Updated comments for items in  Robot Map.

--- src/main/java/org/usfirst/frc/team1360/robot/RobotMap.java
+++ src/main/java/org/usfirst/frc/team1360/robot/RobotMap.java
@@ -28,37 +28,57 @@ public class RobotMap
     * CAN: No
     */
     public static final int DRIVESUBSYSTEM_RIGHT_2 =            3;
-
-
+   
+     /**
+     * IntakeMotor on the Intake Subsystem
+     */
     public static final int INTAKESUBSYSTEM_INTAKE_1 =          4;
-
-
+   
+     /**
+     * ShooterMotor on the Shooter subsystem (same port as IntakeMotor)
+     */
     public static final int SHOOTERSUBSYSTEM_SHOOTER_1 = INTAKESUBSYSTEM_INTAKE_1;
-
-
+  
+     /** 
+     * TiltMotor in the Shooter subsystem
+     */
     public static final int SHOOTERSUBSYSTEM_TILTER =           5;
-
-
+  
     // Solenoids
 
+     /**
+     * Left SolenoidMotor Back on the Drive Subsystem
+     */
     public static final int DRIVESUBSYSTEM_LEFT_SOLENOID_1 =    0;
-
+   
+     /**
+     * Left SolenoidMotor Middle on the Drive Subsystem
+     */
     public static final int DRIVESUBYSTEM_LEFT_SOLENOID_2 =     1;
-
+   
+     /**
+     * Right SolenoidMotor Back on the Drive Subsystem
+     */
     public static final int DRIVESUBSYSTEM_RIGHT_SOLENOID_1 =   2;
-
-    public static final int DRIVESUBSYSTEM_RIGHT_SOLENOID_2 =   3;
-
-    public static final int SHOOTERSUBSYSTEM_SHOOTER_SOLENOID = 4;
-
-
+   
     /**
+     * Right SolenoidMotor Middle on the Dive Subsystem
+     */
+    public static final int DRIVESUBSYSTEM_RIGHT_SOLENOID_2 =   3;
+    
+      /**
+     * ShooterSolenoid on the Shooter Subsystem
+     */
+    public static final int SHOOTERSUBSYSTEM_SHOOTER_SOLENOID = 4;
+  
+     /**
      * Driver Controller
      */
     public static final int JOYSTICK_DRIVER =                   0;
-    /**
+   
+     /**
      * Operator Controller
      */
     public static final int JOYSTICK_OPERATOR =                 1;
-
+   
 }
